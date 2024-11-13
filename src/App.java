a93d3a9c03fbc620876696950f49dee313b0686b
mshirazkamran <blogger31625@gmail.com>
2024-11-13 07:25:42 +0500

    changes

---
+++ src/App.java
@@ -0,0 +1,15 @@
+import ProgramBuilder.ProgramBuilder;
+
+public class App {
+
+    public static void main(String[] args)  {
+       
+        ProgramBuilder.runProgram();
+
+        // String[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+        // System.out.println("Available Fonts:");
+        // for (String font : fonts) {
+        //     System.out.println(font);
+        // }
+    }
+}
---
+++ src/ProgramBuilder/ProgramBuilder.java
@@ -0,0 +1,46 @@
+package ProgramBuilder;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+import java.awt.Font;
+import java.awt.GraphicsEnvironment;
+
+public class ProgramBuilder {
+
+    public static void runProgram()  {
+        
+        System.out.println("Hello, World!");
+
+        JFrame frame = new JFrame("Arabic text");
+
+        frame.setSize(200,200);
+        frame.setLocationRelativeTo(null);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // JLabel label = new JLabel("shiraz kamiran");
+        // label.setFont(new Font("JetBrains Mono Medium", Font.PLAIN, 22));
+        // label.setHorizontalAlignment(SwingConstants.CENTER  + 10);
+    
+        JLabel arabicText = new JLabel("إِذَا ٱلشَّمْسُ كُوِّرَتْ");
+        arabicText.setFont(new Font("Calibri", Font.BOLD, 22));
+        arabicText.setHorizontalAlignment(SwingConstants.CENTER);
+        
+    
+        frame.add(arabicText);
+        // label.setAlignmentX(300.0f);
+        // label.setAlignmentY(400.0f);
+        frame.setVisible(true);
+
+
+        System.out.println("لافد ميدا كومب");
+
+        // String[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+        // System.out.println("Available Fonts:");
+        // for (String font : fonts) {
+        //     System.out.println(font);
+        // }
+    }
+}
